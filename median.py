c7fe4c200e7a505ebcbb90ec53279fe667646467
Shafiul Islam-Mehdi <shafiul.islam-mehdi@kcl.ac.uk>
2022-09-28 16:53:22 +0100

    Implement algorithm to find median of an integer list

--- median.py
+++ median.py
@@ -10,3 +10,14 @@ while True:
     else:
         break
 print(numbers)
+numbers.sort()
+print(numbers)
+if len(numbers) % 2 == 0:
+    middle = (len(numbers) / 2) - 1
+    middle = int(middle)
+    median = (numbers[middle] + numbers[middle + 1]) / 2
+    print(median)
+else:
+    middle = round(len(numbers) / 2)
+    median = numbers[middle]
+    print(median)
